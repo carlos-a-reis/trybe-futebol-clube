0f8c1adc4bf091a53438e483e46fc11dbc90082b
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-09-30 13:44:42 -0300

    Filtragem das partidas em progresso e finalizadas atraves da query string inProgress

--- app/backend/src/controllers/match/match.controller.ts
+++ app/backend/src/controllers/match/match.controller.ts
@@ -2,7 +2,16 @@ import { Request, Response } from 'express';
 import MatchService from '../../services/match/match.service';
 
 class MatchController {
-  static async getAll(_req: Request, res: Response) {
+  static async getAll(req: Request, res: Response) {
+    const { inProgress } = req.query;
+
+    if (inProgress !== undefined) {
+      const status = inProgress === 'true';
+      const matches = await MatchService.getByInProgressStatus(status);
+
+      return res.status(200).json(matches);
+    }
+
     const matches = await MatchService.getAll();
 
     res.status(200).json(matches);
--- app/backend/src/services/match/match.service.ts
+++ app/backend/src/services/match/match.service.ts
@@ -10,6 +10,15 @@ class MatchService {
 
     return matches;
   }
+
+  static async getByInProgressStatus(status: boolean): Promise<Matches[]> {
+    const matches = await Matches.findAll({ include: [
+      { model: Team, as: 'teamHome' },
+      { model: Team, as: 'teamAway' },
+    ] });
+
+    return matches.filter((match) => Boolean(match.inProgress) === status);
+  }
 }
 
 export default MatchService;
